4783b05e733d026c7018c801f48e93dc206ea613
Mia Ineichen <e227419@edu.sbl.ch>
2023-11-23 12:12:06 +0100

    23.11.23
    -Aufgabe 3 und 4 (ausser eine Aufgabe)

--- script.js
+++ script.js
@@ -14,5 +14,33 @@ export function aufgabe01(args) {
 export function aufgabe02(args) {
   const input = args
   return input.toUpperCase();
+  return result
 }
 
+export function aufgabe03(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement ==="e" || currentElement ==="E") {
+      result++
+    }  
+  }
+   return result
+  }
+
+  export function aufgabe04(args) {
+    const input = args
+    let result = 0
+
+    for (let i = 0; i < input.length; i++) {
+      const currentElement = input[i]
+      if (currentElement ===  " " ) {
+        result++
+      }  
+    }
+     return result+1
+    }
+
+   
